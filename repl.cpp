2a727da302b6389b19dc0d5873a7653c50f6f0e0
Desmond Cheong Zhi Xi <desmondcheongzx@gmail.com>
2021-01-14 16:33:05 -0500

    Add sqlite-inspired meta commands
    Replace the 'q' command to quit the repl with ".exit". Now, meta
    commands are identified by a '.' prefix.

--- repl.cpp
+++ repl.cpp
@@ -1,5 +1,15 @@
 #include "utils.h"
 
+typedef enum {
+    META_COMMAND_SUCCESS,
+    META_COMMAND_UNRECOGNIZED
+} MetaCommandResult;
+
+typedef enum {
+    PREPARE_SUCCESS,
+    PREPARE_UNRECOGNIZED
+} PrepareResult;
+
 class Token {
 public:
     char type;
@@ -22,14 +32,16 @@ Token TokenStream::get ()
         return buffer;
     }
     char ch;
+    std::string val;
     std::cin >> ch;
     switch (ch) {
+    case '.': // meta command
+        std::cin >> val;
+        return Token{'.', val};
     case ';':
-    case 'q':
         return Token{ch};
     default:
         std::cin.putback(ch);
-        std::string val;
         std::cin >> val;
         return Token{'.', val};
     }
@@ -44,6 +56,14 @@ void TokenStream::putback (Token t)
     buffer_filled = true;
 }
 
+MetaCommandResult execute_meta_command (Token t)
+{
+    if (t.val == "exit") {
+        exit(0);
+    }
+    return META_COMMAND_UNRECOGNIZED;
+}
+
 TokenStream token_stream;
 
 int main()
@@ -52,11 +72,19 @@ int main()
         try {
             std::cout << "fountainDB> ";
             Token t = token_stream.get();
-            if (t.type == 'q')
-                break;
+            if (t.type == '.') {
+                switch (execute_meta_command(t)) {
+                case (META_COMMAND_SUCCESS):
+                    break;
+                case (META_COMMAND_UNRECOGNIZED):
+                    error("Command is not recognized.\n");
+                }
+                continue;
+            }
             std::cout << t.val << std::endl;
         }
         catch (std::string err) {
+            std::cerr << err << std::endl;
         }
     }
     return 0;
